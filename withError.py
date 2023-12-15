3d9ec38dec5def211e3ec6f7e30c00ef6fc1b33e
HÜMEYRA ÇİMEN <humeyracimen2001@gmail.com>
2023-12-15 15:06:09 +0300

    update

---
+++ withError.py
@@ -0,0 +1,41 @@
+import os
+import random
+
+# Klasördeki tüm txt dosyalarını al
+folder_path = 'C:\Users\HÜMEYRA\Documents\GitHub\KUL\coordinateds2'
+error_folder = 'C:\Users\HÜMEYRA\Documents\GitHub\KUL\errors'
+
+# İfadesiyle belirtilen klasörü oluştur
+os.makedirs(error_folder, exist_ok=True)
+
+file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+
+for file_name in file_names:
+    x_values = []
+    y_values = []
+    
+    # Dosyadan veriyi oku
+    file_path = os.path.join(folder_path, file_name)
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            x, y = map(float, line.split(','))
+            x_values.append(x)
+            y_values.append(y)
+
+    # Rastgele 5 indeks seç
+    selected_indices = random.sample(range(len(y_values)), 50)
+
+    # Her bir seçilen indeks için y değeri rastgele bir şekilde değiştir
+    for index in selected_indices:
+        current_y_value = y_values[index]
+        new_y_value = current_y_value + random.uniform(-5, 5)
+        y_values[index] = new_y_value
+
+    # Hatalı dosyayı aynı adla "errors" klasörüne kaydet
+    error_file_path = os.path.join(error_folder, file_name)
+    
+    with open(error_file_path, 'w') as error_file:
+        for x, y in zip(x_values, y_values):
+            error_file.write(f'{x},{y}\n')
+
+    print(f"Hatalı dosya kaydedildi: {error_file_path}")
