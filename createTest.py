0e5e5222ceb07351eff9f2245dfa19cf0aed3cc1
HÜMEYRA ÇİMEN <humeyracimen2001@gmail.com>
2023-12-19 09:26:10 +0300

    .

--- createTest.py
+++ createTest.py
@@ -1,33 +1,50 @@
-import matplotlib.pyplot as plt
-y_values = []
-x_values = []
+import os
+import random
 
-# read the data from the text file
-with open('smooth2001txt', 'r') as f:
-        for l in f.readlines() :
-                y_values.append(float(l.split(',')[1]))
-                x_values.append(float(l.split(',')[0]))
-y_values2=y_values
-# plot the data
-plt.plot(y_values)
-print(y_values)
-# show the plot
-#plt.show()
+# Klasördeki tüm txt dosyalarını al
+folder_path = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\coordinateds2'
+error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\error'
 
-import random 
+# İfadesiyle belirtilen klasörü oluştur
+os.makedirs(error_folder, exist_ok=True)
+hatalixkonumlari=[]
+file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
 
-# Rastgele 5 indeks seç
-selected_indices = random.sample(range(len(y_values)), 100)
+for file_name in file_names:
+    x_values = []
+    y_values = []
 
-# Her bir seçilen indeks için değeri mevcut değerinden 2 farklı bir şekilde değiştir
-for index in selected_indices:
-    current_value = y_values[index]
-    new_value = current_value + random.uniform(-3, 3) #random.choice([-0.3, 0.3])
-    y_values[index] = new_value
+    # Dosyadan veriyi oku
+    file_path = os.path.join(folder_path, file_name)
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            x, y = map(float, line.split(','))
+            x_values.append(x)
+            y_values.append(y)
 
-# Değiştirilmiş y_values dizisini yazdır
-print("Değiştirilmiş y_values:", y_values)
+    # Rastgele 5 indeks seç
+    error_num = random.randint(0, 10)  # kac hata lı deger olsun 
+    print("error_num:", error_num)
+    selected_indices = random.sample(range(len(y_values)), error_num)
 
+    # Her bir seçilen indeks için y değeri rastgele bir şekilde değiştir
+    for index in selected_indices:
+        current_y_value = y_values[index]
+        error_lim = random.uniform(3, 15)
 
-plt.plot(y_values)
-plt.show()
+        new_y_value = current_y_value + random.uniform(-error_lim, error_lim)
+        y_values[index] = new_y_value
+        hatalixkonumlari.append(x_values[index])
+
+    # Dosya adını güncelle
+    file_name = f"{file_name[:-4]}_error.txt"
+
+    # Hatalı dosyayı aynı adla "errors" klasörüne kaydet
+    error_file_path = os.path.join(error_folder, file_name)
+
+    with open(error_file_path, 'w') as error_file:
+        for x, y in zip(x_values, y_values):
+            error_file.write(f'{x},{y}\n')
+
+    print(f"Hatalı dosya kaydedildi: {error_file_path}")
+    print("hatalixkonumlari:",hatalixkonumlari)
--- withError.py
+++ withError.py
@@ -3,14 +3,14 @@ import random
 
 # Klasördeki tüm txt dosyalarını al
 folder_path = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\coordinateds2'
-error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\trueValues'
+error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\falseValues2'
 
 # İfadesiyle belirtilen klasörü oluştur
 os.makedirs(error_folder, exist_ok=True)
 
 file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
 
-for i in range(500):
+for i in range(1000):
     for file_name in file_names:
         x_values = []
         y_values = []
@@ -24,13 +24,13 @@ for i in range(500):
                 y_values.append(y)
         
         # Rastgele 5 indeks seç
-        error_num = random.randint(0, 50)
+        error_num = random.randint(0, 50)#kac hata lı deger olsun 
         selected_indices = random.sample(range(len(y_values)), error_num)
 
         # Her bir seçilen indeks için y değeri rastgele bir şekilde değiştir
         for index in selected_indices:
             current_y_value = y_values[index]
-            error_lim = random.randint(0, 3)
+            error_lim = random.randint(3, 15)
 
             new_y_value = current_y_value + random.uniform(-error_lim, error_lim)
             y_values[index] = new_y_value
