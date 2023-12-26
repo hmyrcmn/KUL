cef3d0163730c498d4aa367cf4dad188eecb0c54
HÜMEYRA ÇİMEN <humeyracimen2001@gmail.com>
2023-12-26 10:02:33 +0300

    aaa

--- withError.py
+++ withError.py
@@ -3,14 +3,14 @@ import random
 
 # Klasördeki tüm txt dosyalarını al
 folder_path = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\coordinateds2'
-error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\data\\falseValues'
+error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\data\\testmodel\\true'
 
 # İfadesiyle belirtilen klasörü oluştur
 os.makedirs(error_folder, exist_ok=True)
 
 file_names = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
 
-for i in range(1000):
+for i in range(5):
     for file_name in file_names:
         x_values = []
         y_values = []
@@ -30,7 +30,7 @@ for i in range(1000):
         # Her bir seçilen indeks için y değeri rastgele bir şekilde değiştir
         for index in selected_indices:
             current_y_value = y_values[index]
-            error_lim = random.randint(4,10)
+            error_lim = random.randint(0,3)
 
             new_y_value = current_y_value + random.uniform(-error_lim, error_lim)
             y_values[index] = new_y_value
