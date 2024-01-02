7d81428c566570971657199f73d1685b98959853
HÜMEYRA ÇİMEN <humeyracimen2001@gmail.com>
2024-01-02 11:42:55 +0300

    up data

--- withError.py
+++ withError.py
@@ -2,8 +2,8 @@ import os
 import random
 
 # Klasördeki tüm txt dosyalarını al
-folder_path = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\coordinateds2'
-error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\coordinateds2\\test\\trueValues'
+folder_path = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\BigData'
+error_folder = 'C:\\Users\\HÜMEYRA\\Documents\\GitHub\\KUL\\BigData\\ORJ\\true'
 
 # İfadesiyle belirtilen klasörü oluştur
 os.makedirs(error_folder, exist_ok=True)
@@ -26,13 +26,13 @@ for i in range(5):
                 say+=1
         
         # Rastgele 5 indeks seç
-        error_num = random.randint(0, 30)#kac hata lı deger olsun 
+        error_num = random.randint(0, 10)#kac hata lı deger olsun 
         selected_indices = random.sample(range(len(y_values)), error_num)
 
         # Her bir seçilen indeks için y değeri rastgele bir şekilde değiştir
         for index in selected_indices:
             current_y_value = y_values[index]
-            error_lim = random.randint(0,2)
+            error_lim = random.randint(0,3)
 
             new_y_value =current_y_value + random.uniform(error_lim, error_lim)
             y_values[index] = new_y_value
